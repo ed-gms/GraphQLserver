cd98f516e5d8b4e6dbb360f9efdc76c7aa510411
ed-silva <32685663+ed-silva@users.noreply.github.com>
2018-07-09 14:16:42 -0700

    adding GraphQL

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema');
 const app = express();
 const port = process.argv[2] || 8080;
+
+app.get('/', (req, res) => {
+  res.send("It's working!")
+})
+
+const root = { hello: () => "Hello, welcome to GraphQL!" }
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(port, (req, res) => {
   console.log(`Listening on port ${port}`);
